ce0945e641677137a22f6be2a80dd6277e5e383a
Leo <lechewu@gmail.com>
2023-05-22 22:04:38 +0800

    feat: apply Navbar and Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { useCallback, useState } from 'react'
+import { Navbar, Sidebar } from 'src/components'
+import { navItems } from 'src/settings'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +17,33 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [isSidebarOpen, setSidebarStatus] = useState(false)
+  const onMenuButtonClick = useCallback(() => {
+    setSidebarStatus(!isSidebarOpen)
+  }, [isSidebarOpen])
+
+  const closeSidbar = useCallback(() => {
+    setSidebarStatus(false)
+  }, [setSidebarStatus])
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid min-h-screen grid-rows-header">
+          <div>
+            <Navbar onMenuButtonClick={onMenuButtonClick} />
+          </div>
+
+          <div className="grid md:grid-cols-sidebar">
+            <Sidebar
+              open={isSidebarOpen}
+              closeSidbar={closeSidbar}
+              navItems={navItems}
+            />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
