299532701a97db5122aa66ad169c598252a8ee02
Leo <lechewu@gmail.com>
2023-05-22 22:04:01 +0800

    feat: init settings

---
+++ src/settings/navItems.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  RocketLaunchIcon,
+  BeakerIcon,
+  FireIcon,
+  BriefcaseIcon,
+} from '@heroicons/react/24/outline'
+import { NavItem } from 'src/components'
+
+export const navItems: NavItem[] = [
+  {
+    label: 'Breaking News',
+    href: '/',
+    icon: <FireIcon className="w-6 h-6" />,
+  },
+  {
+    label: 'Business',
+    href: '/business',
+    icon: <BriefcaseIcon className="w-6 h-6" />,
+  },
+  {
+    label: 'Science',
+    href: '/science',
+    icon: <BeakerIcon className="w-6 h-6" />,
+  },
+  {
+    label: 'Technology',
+    href: '/technology',
+    icon: <RocketLaunchIcon className="w-6 h-6" />,
+  },
+]
