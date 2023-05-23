9ce7ff7d6784c122cd6614e0b50ff7e2884660c6
Leo <lechewu@gmail.com>
2023-05-23 17:55:25 +0800

    feat: disable rewrites

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/business',
-        destination: '/',
-      },
-      {
-        source: '/science',
-        destination: '/',
-      },
-      {
-        source: '/technology',
-        destination: '/',
-      },
-    ]
-  },
+const nextConfig = {
+  output: 'export',
+  // below confilt with output: 'export',
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/business',
+  //       destination: '/',
+  //     },
+  //     {
+  //       source: '/science',
+  //       destination: '/',
+  //     },
+  //     {
+  //       source: '/technology',
+  //       destination: '/',
+  //     },
+  //   ]
+  // },
 }
+
+module.exports = nextConfig
