442c9dddaca7b50511938a8eb5a31c51c13b48ec
Leo <lechewu@gmail.com>
2023-05-23 14:47:22 +0800

    feat: add News

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
 'use client'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
 import { useNews } from 'src/hooks'
+import { pathToLabel } from 'src/settings'
 
 export default function Home() {
+  const pathname = usePathname()
   const { news } = useNews()
 
-  return <div>{JSON.stringify(news)}</div>
+  const News = useMemo(
+    () =>
+      news.map(({ source: { name }, title, url, publishedAt }) => {
+        return (
+          <a
+            key={url}
+            href={url}
+            target="_blank"
+            className="block mb-4 pb-2 border-b-2 border-blue-900"
+          >
+            <div className="text-base">{title}</div>
+            <div className="text-sm">
+              <span>{name}, </span>
+              <span>{new Date(publishedAt).toLocaleDateString('zh-TW')}</span>
+            </div>
+          </a>
+        )
+      }),
+    [news],
+  )
+
+  return (
+    <div className="md:max-w-[calc(100vw-300px)] px-[20px]">
+      <h1 className="text-xl mb-2">{pathToLabel.get(pathname)}</h1>
+      <div>{News}</div>
+    </div>
+  )
 }
