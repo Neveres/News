aa9987825170c9eaca08c7742f2d8cee09289615
Leo <lechewu@gmail.com>
2023-05-29 20:56:53 +0800

    feat: update config

--- src/settings/httpClientConfigure.ts
+++ src/settings/httpClientConfigure.ts
@@ -1,5 +1,6 @@
-export const baseURL = 'https://newsapi.org/v2/top-headlines'
+export const baseURL = 'https://newsapi.org/v2/top-headlines?'
 
 export const defaultParams = {
   country: 'tw',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
 }
